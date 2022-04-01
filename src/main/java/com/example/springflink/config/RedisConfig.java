4167850eb8d9ef29e48e1bd00443d042f6ea82c5
wangzuoyu1 <wangzuoyu1@jd.com>
2022-04-01 17:33:21 +0800

    add redis 分布式限流～

--- src/main/java/com/example/springflink/config/RedisConfig.java
+++ src/main/java/com/example/springflink/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.example.springflink.config;
 
+import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,11 +8,16 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.stereotype.Component;
+
 
 /**
  * @author wangzuoyu1
@@ -21,30 +27,71 @@ import org.springframework.stereotype.Component;
 @Data
 @ConfigurationProperties("spring.redis")
 @EnableConfigurationProperties
-@Component
+@Configuration
 public class RedisConfig {
     private String host;
     private int port;
 
-    @Bean(name = "redisClient")
+    @Bean(name = "redisTemplate")
+    @Primary
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-        ObjectMapper om = new ObjectMapper();
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        jackson2JsonRedisSerializer.setObjectMapper(om);
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        // key采用String的序列化方式
-        template.setKeySerializer(stringRedisSerializer);
-        // hash的key也采用String的序列化方式
-        template.setHashKeySerializer(stringRedisSerializer);
-        // value序列化方式采用jackson
-        template.setValueSerializer(jackson2JsonRedisSerializer);
-        // hash的value序列化方式采用jackson
-        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+
+        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
+        Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        serializer.setObjectMapper(mapper);
+
+        template.setValueSerializer(serializer);
+        //使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
         template.afterPropertiesSet();
         return template;
     }
+
+    @Bean(name = "reactiveRedisTemplate")
+    @Primary
+    public ReactiveRedisTemplate<String, Object> reactiveRedisTemplate(LettuceConnectionFactory connectionFactory) {
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        GenericFastJsonRedisSerializer fastJsonRedisSerializer = new GenericFastJsonRedisSerializer();
+        RedisSerializationContext.SerializationPair<String> keySerializationPair =
+            RedisSerializationContext.SerializationPair.fromSerializer(stringRedisSerializer);
+        RedisSerializationContext.SerializationPair<Object> valueSerializationPair =
+            RedisSerializationContext.SerializationPair.fromSerializer(fastJsonRedisSerializer);
+        RedisSerializationContext.SerializationPair<Object> hashValueSerializationPair =
+            RedisSerializationContext.SerializationPair.fromSerializer(fastJsonRedisSerializer);
+        RedisSerializationContext<String, Object> context = new RedisSerializationContext<String, Object>() {
+            @Override
+            public SerializationPair getKeySerializationPair() {
+                return keySerializationPair;
+            }
+
+            @Override
+            public SerializationPair getValueSerializationPair() {
+                return valueSerializationPair;
+            }
+
+            @Override
+            public SerializationPair getHashKeySerializationPair() {
+                return keySerializationPair;
+            }
+
+            @Override
+            public SerializationPair getHashValueSerializationPair() {
+                return hashValueSerializationPair;
+            }
+
+            @Override
+            public SerializationPair<String> getStringSerializationPair() {
+                return keySerializationPair;
+            }
+        };
+        return new ReactiveRedisTemplate<>(connectionFactory, context);
+    }
+
 }
+
--- src/main/java/com/example/springflink/server/Builder.java
+++
@@ -1,8 +0,0 @@
-package com.example.springflink.server;
-
-/**
- * @author wangzuoyu1
- */
-public class Builder {
-
-}
